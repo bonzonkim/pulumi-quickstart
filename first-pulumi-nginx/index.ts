a6585e32f9716508eb4a26348526d779695a4444
Kelly Kang <flrhyme9@gmail.com>
2024-04-30 16:52:18 +0900

    initial

---
+++ first-pulumi-nginx/index.ts
@@ -0,0 +1,44 @@
+import * as k8s from "@pulumi/kubernetes";
+
+const appLabels = { app: "nginx" };
+const appName = "nginx";
+
+const deployment = new k8s.apps.v1.Deployment(appName, {
+  metadata: { namespace: "test" },
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels, namespace: "test" },
+            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+        }
+    }
+});
+
+const service = new k8s.core.v1.Service(appName, {
+  metadata: {
+    name: "svc-nginx",
+    namespace: "test",
+    labels: appLabels
+  },
+  spec: {
+    type: "NodePort",
+    ports: [{
+      port: 80,
+      targetPort: 80,
+      nodePort: 30020
+    }],
+    selector: appLabels
+  },
+});
+
+//const frontend = new k8s.core.v1.Service(appName, {
+//  metadata: { labels: deployment.spec.template.metadata.labels },
+//  spec: {
+//    type: "NodePort",
+//    ports: [{ port: 80, targetPort: 80, nodePort: 30202, protocol: "TCP" }],
+//    selector: appLabels,
+//  },
+//});
+
+//export const name = deployment.metadata.name;
