ebb4528fd42d2700d50099c7063fa18adc765212
Kelly Kang <flrhyme9@gmail.com>
2024-05-02 18:08:02 +0900

    feat: node-selector, postgre persistence

--- zabbix/index.ts
+++ zabbix/index.ts
@@ -1,69 +1,70 @@
 import * as kubernetes from "@pulumi/kubernetes";
 
 // Deploying the zabbix-server-mysql Helm chart to a Kubernetes cluster.
-const zabbixChart = new kubernetes.helm.v3.Chart("zabbix-test", {
-    chart: "zabbix",
-    version: "4.3.0", // Replace with the appropriate chart version
-    fetchOpts: {
-        repo: "https://zabbix-community.github.io/helm-zabbix",
+const zabbixChart = new kubernetes.helm.v3.Chart("test", {
+  chart: "zabbix",
+  version: "4.3.0", // Replace with the appropriate chart version
+  namespace: "test",
+  fetchOpts: {
+    repo: "https://zabbix-community.github.io/helm-zabbix",
+  },
+  // If needed, you can provide custom values by specifying the `values` property.
+  values: {
+    nodeSelector: {
+      "worker": "worker1",
     },
-    // If needed, you can provide custom values by specifying the `values` property.
-     values: {
-        zabbixImageTag: "ubuntu-6.0.20",
-        postgresAccess: {
-          useUnifiedSecret: true,
-          unifiedSecretName: "zabbixdb-pguser-zabbix",
-          unifiedSecretAutoCreate: true,
-          host: "zabbix-postgresql",
-          port: "5432",
-          user: "zabbix",
-          password: "egarreo01",
-          database: "zabbix"
-        },
-         zabbixServer: {
-             enabled: true,
-             replicaCount: 1,
-             image: {
-              repository: "zabbix/zabbix-server-pgsql",
-              tag: null,
-              pullPolicy: "IfNotPresent"
-             },
-             service: {
-                 type: "NodePort",
-                 ports: {
-                    port: 80,
-                    targetPort: 10051,
-                    nodePort: 31051
-                  },
-             },
-         },
-         postgresql: {
-          eabled: true,
-          image: {
-            repository: "postgres",
-            tag: 15,
-            pullPolicy: "IfNotPresent",
+    zabbixImageTag: "ubuntu-6.0.20",
+    postgresAccess: {
+      useUnifiedSecret: true,
+      unifiedSecretName: "zabbixdb-pguser-zabbix",
+      unifiedSecretAutoCreate: true,
+      host: "zabbix-postgresql",
+      port: "5432",
+      user: "zabbix",
+      password: "password",
+      database: "zabbix",
+    },
+    zabbixServer: {
+      enabled: true,
+      replicaCount: 1,
+      image: {
+        repository: "zabbix/zabbix-server-pgsql",
+        tag: null,
+        pullPolicy: "IfNotPresent",
       },
-          service: {
-             type: "ClusterIP",
-             port: 5432,
-            },
-          },
-        zabbixWeb: {
-          enables: true,
-          replicaCount: 1,
-          image: {
-            repository: "zabbix/zabbix-web-nginx-pgsql",
-            tag: null,
-            pullPolicy: "IfNotPresent"
-      },
-         service: {
-          type: "NodePort",
-          port: 80,
-          targetPort: 80,
-          nodePort: 31080
+      service: {
+        type: "NodePort",
+        port: 80,
+        targetPort: 10051,
+        nodePort: 31056,
       },
     },
-          
-         }
+    postgresql: {
+      enabled: true,
+      image: {
+        repository: "postgres",
+        tag: 15,
+        pullPolicy: "IfNotPresent",
+      },
+      service: {
+        type: "ClusterIP",
+        port: 5432,
+      },
+    },
+    zabbixWeb: {
+      enabled: true,
+      replicaCount: 1,
+      image: {
+        repository: "zabbix/zabbix-web-nginx-pgsql",
+        tag: null,
+        pullPolicy: "IfNotPresent",
+      },
+      service: {
+        type: "NodePort",
+        port: 80,
+        targetPort: 80,
+        nodePort: 31082,
+      },
+    },
+  },
 });
